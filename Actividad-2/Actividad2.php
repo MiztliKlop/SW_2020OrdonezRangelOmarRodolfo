1e5ee470b41dcbeeae5ffdf049a394158cd8b7ec
MiztliKlop <legocity517@gmail.com>
2020-05-23 18:29:16 -0500

    FEAT: Corrección Actividad 2

---
+++ Actividad-2/Actividad2.php
@@ -0,0 +1,3 @@
+<?php
+echo "Omar Rodolfo Ordóñez Rangel. <br> <br> Cosas que he aprendido y me han gustado: <br><br> 1.- A mezclar php con html. <br> <br> 2.- Conectar en GitHub colaboradores."
+ ?>
